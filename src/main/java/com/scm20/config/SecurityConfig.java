167dd37ffe8d238c3739722a5891801dad80a2b0
sherkepriyanshu <priyanshu190310@gmail.com>
2024-09-11 16:11:04 +0530

    service package updated

--- src/main/java/com/scm20/config/SecurityConfig.java
+++ src/main/java/com/scm20/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.scm20.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,6 +11,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 // import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import com.scm20.services.impl.SecurityCustomUserDetailService;
 
 @Configuration
 public class SecurityConfig {
@@ -39,13 +42,15 @@ public class SecurityConfig {
     //     return  inMemoryUserDetailsManager;
     // }
 
+    @Autowired
+    private SecurityCustomUserDetailService userDetailService;
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider()
     {
          DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         
-        daoAuthenticationProvider.setUserDetailsService(null);
+        daoAuthenticationProvider.setUserDetailsService(userDetailService);
 
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
--- src/main/java/com/scm20/services/impl/SecurityCustomUserDetailService.java
+++ src/main/java/com/scm20/services/impl/SecurityCustomUserDetailService.java
@@ -1,11 +1,13 @@
-package com.scm20.config;
+package com.scm20.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.scm20.repository.UserRepo;
-
+@Service
 public class SecurityCustomUserDetailService implements UserDetailsService {
 
     @Autowired
